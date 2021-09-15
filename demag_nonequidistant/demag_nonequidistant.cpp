faa71637ffc3257b93489a01c02c700753281910
Paul Heistracher <paul.thomas.heistracher@univie.ac.at>
2021-09-15 03:22:27 +0200

    demag_nonequidistant.cpp  fix most conversion warnings, making it c++11 compatible, using std::size_t throughout

--- demag_nonequidistant/demag_nonequidistant.cpp
+++ demag_nonequidistant/demag_nonequidistant.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#define BOOST_BIND_GLOBAL_PLACEHOLDERS // surpress warning
 #include <boost/python.hpp>
 #include <boost/python/numpy.hpp>
 #include <cassert>
@@ -13,20 +14,21 @@ struct NonequiMesh {
   double dx, dy;  //!< Distance between equidistant x, y cells in [m]
   std::vector<double>
       z_spacing; //!< Thickness of each layer along z-axis in [m]
+  std::size_t nz() const { return z_spacing.size(); }
 };
 
-} // namespace magnumaf
-
-namespace magnumaf::util {
-inline int ij2k(const int i, const int j, const int n) {
+namespace util {
+// inline int ij2k(const int i, const int j, const int n) {
+inline long int ij2k(long int i, long int j, long int n) {
+  // template <typename T> T ij2k(const T i, const T j, const T n) {
   return (n * (n + 1) / 2) - (n - i) * ((n - i) + 1) / 2 + j - i;
 }
-} // namespace magnumaf::util
+} // namespace util
 
-namespace magnumaf::newell_nonequi {
+namespace newell_nonequi {
 
-inline unsigned nx_expanded(unsigned nx) { return 2 * nx; }
-inline unsigned ny_expanded(unsigned ny) { return 2 * ny; }
+inline std::size_t nx_expanded(std::size_t nx) { return 2 * nx; }
+inline std::size_t ny_expanded(std::size_t ny) { return 2 * ny; }
 
 double f(double x, double y, double z) {
   x = std::fabs(x);
@@ -145,7 +147,7 @@ double Nxy(const double x, const double y, const double z, const double dx,
 }
 
 double nonequi_index_distance(const std::vector<double> &spacings,
-                              const unsigned i, const unsigned j,
+                              const std::size_t i, const std::size_t j,
                               const bool verbose) {
   // Calculates the signed distance beween elements by summing up i < j:
   // sum_(k=i)^(j-1)[spacings[k]] or i > j: sum_(k=j)^(i-1)[ - spacings[k]]
@@ -160,11 +162,11 @@ double nonequi_index_distance(const std::vector<double> &spacings,
 
   double result = 0;
   if (i > j) {
-    for (unsigned k = i; k > j; k--) {
+    for (std::size_t k = i; k > j; k--) {
       result -= spacings[k - 1];
     }
   } else {
-    for (unsigned k = i; k < j; k++) {
+    for (std::size_t k = i; k < j; k++) {
       result += spacings[k];
     }
   }
@@ -172,29 +174,26 @@ double nonequi_index_distance(const std::vector<double> &spacings,
 }
 
 void init_N(const NonequiMesh &nemesh, std::vector<double> &N,
-            unsigned ix_start, unsigned ix_end) {
-  for (unsigned ix = ix_start; ix < ix_end; ix++) {
-    const int jx = (ix + nx_expanded(nemesh.nx) / 2) % nx_expanded(nemesh.nx) -
-                   nx_expanded(nemesh.nx) / 2;
-    for (unsigned iy = 0; iy < ny_expanded(nemesh.ny); iy++) {
-      const int jy =
+            std::size_t ix_start, std::size_t ix_end) {
+  for (std::size_t ix = ix_start; ix < ix_end; ix++) {
+    const long int jx =
+        (ix + nx_expanded(nemesh.nx) / 2) % nx_expanded(nemesh.nx) -
+        nx_expanded(nemesh.nx) / 2;
+    for (std::size_t iy = 0; iy < ny_expanded(nemesh.ny); iy++) {
+      const long int jy =
           (iy + ny_expanded(nemesh.ny) / 2) % ny_expanded(nemesh.ny) -
           ny_expanded(nemesh.ny) / 2;
-      for (unsigned i_source = 0; i_source < nemesh.z_spacing.size();
-           i_source++) {
-        for (unsigned i_target = 0; i_target < nemesh.z_spacing.size();
-             i_target++) {
+      for (std::size_t i_source = 0; i_source < nemesh.nz(); i_source++) {
+        for (std::size_t i_target = 0; i_target < nemesh.nz(); i_target++) {
 
           if (i_source <= i_target) {
-            const int idx =
-                6 *
-                (util::ij2k(i_source, i_target, nemesh.z_spacing.size()) +
-                 ((nemesh.z_spacing.size() * (nemesh.z_spacing.size() + 1)) /
-                  2) *
-                     (iy + ny_expanded(nemesh.ny) * ix));
+            const long int idx =
+                6 * (util::ij2k(i_source, i_target, nemesh.nz()) +
+                     ((nemesh.nz() * (nemesh.nz() + 1)) / 2) *
+                         (iy + ny_expanded(nemesh.ny) * ix));
             // std::cout << "idx=" << idx << " of " <<
             // nx_expanded(nemesh.nx) * ny_expanded(nemesh.ny) *
-            // (nemesh.z_spacing.size() * (nemesh.z_spacing.size() + 1))/2 * 6
+            // (nemesh.nz() * (nemesh.nz() + 1))/2 * 6
             // << std::endl;
             const double x = nemesh.dx * static_cast<double>(jx);
             const double y = nemesh.dy * static_cast<double>(jy);
@@ -227,62 +226,54 @@ void init_N(const NonequiMesh &nemesh, std::vector<double> &N,
 }
 
 std::vector<double> calculate_N(const NonequiMesh &nemesh,
-                                unsigned nthreads_in) {
-  unsigned nthreads =
+                                std::size_t nthreads_in) {
+  std::size_t nthreads =
       nthreads_in > 0 ? nthreads_in : std::thread::hardware_concurrency();
   assert(nthreads > 0);
 
-  std::vector<double> N_values(
-      nx_expanded(nemesh.nx) * ny_expanded(nemesh.ny) *
-      (nemesh.z_spacing.size() * (nemesh.z_spacing.size() + 1)) / 2 * 6);
+  std::vector<double> N_values(nx_expanded(nemesh.nx) * ny_expanded(nemesh.ny) *
+                               (nemesh.nz() * (nemesh.nz() + 1)) / 2 * 6);
 
-  std::vector<std::thread> t;
-  for (unsigned i = 0; i < nthreads; i++) {
-    unsigned ix_start =
+  std::vector<std::thread> threads;
+  for (std::size_t i = 0; i < nthreads; i++) {
+    std::size_t ix_start =
         i * static_cast<double>(nx_expanded(nemesh.nx)) / nthreads;
-    unsigned ix_end =
+    std::size_t ix_end =
         (i + 1) * static_cast<double>(nx_expanded(nemesh.nx)) / nthreads;
-    t.emplace_back(init_N, std::ref(nemesh), std::ref(N_values), ix_start,
-                   ix_end);
+    threads.emplace_back(init_N, std::ref(nemesh), std::ref(N_values), ix_start,
+                         ix_end);
   }
 
-  for (unsigned i = 0; i < nthreads; i++) {
-    t[i].join();
+  for (auto &thread : threads) {
+    thread.join();
   }
 
   return N_values;
+}
 
   // TODO add option returning fft transform if arrayfire is installed
 
-  // af::array Naf(6, (nemesh.z_spacing.size() * (nemesh.z_spacing.size() +
+  // af::array Naf(6, (nemesh.nz() * (nemesh.nz() +
   // 1)) / 2, ny_expanded(nemesh.ny), nx_expanded(nemesh.nx),
   //               N_values.data());
   // Naf = af::reorder(Naf, 3, 2, 1, 0);
   // Naf = af::fftR2C<2>(Naf);
   // return Naf;
-}
 
-} // namespace magnumaf::newell_nonequi
+} // namespace newell_nonequi
+} // namespace magnumaf
 
 namespace bn = boost::python::numpy;
 namespace bp = boost::python;
 
-// const auto double_vec_from_pylist = [](bp::list &list_of_doubles) {
-//   std::vector<double> result{};
-//   for (size_t i = 0; i < bp::len(list_of_doubles); ++i) {
-//     result.push_back(bp::extract<double>(list_of_doubles[i]));
-//   }
-//   return result;
-// };
-
-const auto vec_from_pylist =
-    []<typename T = double>(bp::list & list_of_doubles) {
+// const auto vec_from_pylist = []<typename T>(bp::list &values) {
+template <typename T> std::vector<T> vec_from_pylist(bp::list &values) {
   std::vector<T> result{};
-  for (bp::ssize_t i = 0; i < bp::len(list_of_doubles); ++i) {
-    result.push_back(bp::extract<T>(list_of_doubles[i]));
+  for (bp::ssize_t i = 0; i < bp::len(values); ++i) {
+    result.push_back(bp::extract<T>(values[i]));
   }
   return result;
-};
+}
 
 bn::ndarray cp_vec_to_ndarray(std::vector<double> const &v,
                               std::vector<Py_intptr_t> const &shape) {
@@ -292,21 +283,34 @@ bn::ndarray cp_vec_to_ndarray(std::vector<double> const &v,
   return result;
 }
 
+template <typename From, typename To>
+std::vector<To> statcast_each(std::initializer_list<From> &&from) {
+  std::vector<To> to;
+  for (auto elem : from) {
+    to.emplace_back(static_cast<To>(elem));
+  }
+  return to;
+}
+
 bn::ndarray setup_demagtensor(size_t nx, size_t ny, double dx, double dy,
                               bp::list &dz_list) {
 
-  auto dz_values = vec_from_pylist(dz_list);
+  auto dz_values = vec_from_pylist<double>(dz_list);
+  // auto dz_values = vec_from_pylist.operator()<double>(dz_list);
 
-  const auto nemesh = magnumaf::NonequiMesh{
-      .nx = nx, .ny = ny, .dx = dx, .dy = dy, .z_spacing = dz_values};
+  const auto nemesh = magnumaf::NonequiMesh{nx, ny, dx, dy, dz_values};
   auto N = magnumaf::newell_nonequi::calculate_N(
       nemesh, std::thread::hardware_concurrency());
 
   const auto nx_exp = magnumaf::newell_nonequi::nx_expanded(nemesh.nx);
   const auto ny_exp = magnumaf::newell_nonequi::ny_expanded(nemesh.ny);
-  const auto nz = nemesh.z_spacing.size();
+  const auto nz = nemesh.nz();
 
-  std::vector<Py_intptr_t> shape = {nx_exp, ny_exp, (nz * (nz + 1)) / 2, 6};
+  // std::vector<Py_intptr_t> shape = {
+  //     nx_exp, ny_exp, static_cast<Py_intptr_t>(nz * (nz + 1)) / 2, 6};
+
+  std::vector<Py_intptr_t> shape = statcast_each<std::size_t, Py_intptr_t>(
+      {nx_exp, ny_exp, (nz * (nz + 1)) / 2, 6});
 
   const auto result = cp_vec_to_ndarray(N, shape);
   return result;
