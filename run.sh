858da3d68dc522040152a17b12927a6a64a94a4c
Paul Heistracher <paul.thomas.heistracher@univie.ac.at>
2021-09-15 03:23:46 +0200

    Move CMakeLists.txt to root dir, as is default

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/bash -e
 
 cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) # make sure we are in correct dir
 
 mkdir -p build
 [ "$1" == "-r" ] && rm -r build/*
 
-(cd build && cmake ../demag_nonequidistant && make -j)
+(cd build && cmake .. && make -j)
 
 PYTHONPATH=build/:${PYTHONPATH} python3 test.py
--- test.py
+++ test.py
@@ -1,30 +1,19 @@
 import demag_nonequidistant as dn
 
-# N_demag = dn.setup_demagtensor(nx = 2, ny = 3, dx = 1e-9, dy = 2e-9,
-#         dz_list = [1e-9, 2e-9, 3e-9, 4e-9, 5e-9, 6e-9])
-
-nx, ny = 5, 7
-dx, dy =  1.5e-9, 2.2e-9
-z_spacing = [1e-9, 2e-9, 3e-9, 4e-9, 5e-9, 6e-9, 1e-9, 2e-9, 3e-9, 4e-9, 5e-9, 6e-9]
-
-# nx, ny = 2, 3
-# dx, dy =  1.e-9, 2e-9
-# z_spacing = [1e-9, 2e-9, 3e-9, 4e-9, 5e-9, 6e-9]
+if False:
+    nx, ny = 5, 7
+    dx, dy =  1.5e-9, 2.2e-9
+    z_spacing = [1e-9, 2e-9, 3e-9, 4e-9, 5e-9, 6e-9, 1e-9, 2e-9, 3e-9, 4e-9, 5e-9, 6e-9]
+else:
+    nx, ny = 2, 3
+    dx, dy =  1.e-9, 2e-9
+    z_spacing = [1e-9, 2e-9, 3e-9, 4e-9, 5e-9, 6e-9]
 
 N_demag = dn.setup_demagtensor(nx = nx, ny = ny, dx = dx, dy = dy,
         dz_list = z_spacing)
 
 # print(N_demag)
 print(N_demag.shape)
-
-import arrayfire as af
-af_N_demag = af.from_ndarray(N_demag)
-# print(1e30 * af_N_demag)
-N_fft = af.fft2_r2c(af_N_demag)
-# print(1e30 * N_fft)
-print(N_fft.shape)
-
-# ==== magnumaf ====
 
 def module_exists(module_name):
     try:
@@ -34,12 +23,25 @@ def module_exists(module_name):
     else:
         return True
 
+if module_exists('arrayfire'):
+    import arrayfire as af
+    af_N_demag = af.from_ndarray(N_demag)
+    # print(1e30 * af_N_demag)
+    N_fft = af.fft2_r2c(af_N_demag)
+    # print(1e30 * N_fft)
+    print("N_fft.shape  ", N_fft.shape)
+
+# ==== magnumaf ====
+
 if module_exists('magnumaf'):
     import magnumaf as maf
     mesh = maf.NonequiMesh(nx, ny, dx, dy, z_spacing)
-    demag = maf.NonequiDemagField(mesh, verbose = True, caching = False, nthreads = 6)
+    demag = maf.NonequiDemagField(mesh, verbose = False, caching = False, nthreads = 6)
     mafNfft = demag.get_Nfft()
     # print(1e30 * mafNfft)
-    print(mafNfft.shape)
-    
-    print(af.max(N_fft - mafNfft))
+    print("mafNfft.shape", mafNfft.shape)
+    maxdiff = af.max(N_fft - mafNfft)
+    print("maxdiff=", maxdiff)
+
+    if maxdiff != 0.0:
+        raise ValueError("N_fft values differ!, maxdiff is", maxdiff, "but should be 0.0")
